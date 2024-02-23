23a6dfd6dfe55117b759542ddac455fa704f75c6
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-23 10:36:25 +0100

    add 0-

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ * Description: '_putchar
+ * Return: Always 1 (Success)
+ */
+
+int main(char c)
+{
+	c = 'a';
+	_putchar(c);
+	return (-1);
+}
---
+++ functions_nested_loops/putchar.h
@@ -0,0 +1,7 @@
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
+
+int _putchar(void);
+char c;
+
+#endif

e4f057709c6b64aa476633e9a68b1a0ead1cbbbf
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-23 12:47:16 +0100

    fix bug

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+* print_alphabet - print alphabet
+*
+* Return: Always 0
+*/
+
 void print_alphabet(void)
 {
 	char lettre;
+
 	for (lettre = 'a'; lettre <= 'z'; lettre++)
 		_putchar(lettre);
 	_putchar('\n');

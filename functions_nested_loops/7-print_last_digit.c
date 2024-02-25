8cd9f764c7a8089e0648ffd57fadc44d13acb9d8
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-25 16:19:14 +0100

    add 7-

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - last digit
+* @n: number
+* Return: Always 0
+*/
+
+int print_last_digit(int n)
+{
+	unsigned int digit;
+
+	digit = (n % 10);
+	return (digit);
+}

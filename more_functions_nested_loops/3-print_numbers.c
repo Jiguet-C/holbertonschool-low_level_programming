0a06a2a8feeab2a6d3c8a5c2e5c5070ffc3e30a9
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-26 16:14:51 +0100

    add 3-

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers
+* Return: Always 0
+*/
+
+void print_numbers(void)
+{
+	int nombre;
+	for(nombre = '0'; nombre <= '9'; nombre++)
+		_putchar(nombre);
+
+	_putchar('\n');
+}

38b8d17e2691a28227efbda027be1516e3b072ea
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-27 16:35:47 +0100

    add 2-

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - length of a string
+* @s: string
+* Return: number of char
+*/
+
+int _strlen(char *s)
+{
+	int nombreDeCaracteres;
+
+	for (nombreDeCaracteres = 0 ; s[nombreDeCaracteres] != '\0' ; nombreDeCaracteres++)
+	{
+	}
+
+	return (nombreDeCaracteres);
+}

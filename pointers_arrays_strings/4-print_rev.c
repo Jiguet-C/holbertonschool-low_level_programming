6a7ddf375bacb1150948a2cc81a78883950a8221
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-27 16:53:42 +0100

    add 4-

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_rev - prints in reverse
+* @s: string
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int nombreCaract;
+
+	for (nombreCaract = 0 ; s[nombreCaract] != '\0' ; nombreCaract++)
+	{
+	}
+	for (i = nombreCaract ; i >= 0 ; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}

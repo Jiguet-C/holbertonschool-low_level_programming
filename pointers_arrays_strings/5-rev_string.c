6f1bbfee853b64f65fc57403427d30f642ae8d37
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-28 09:36:41 +0100

    add 5-

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - string in reverse
+* @s: string
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int nombreCaract;
+	char temporaire[200];
+
+	for (nombreCaract = 0 ; s[nombreCaract] != '\0' ; nombreCaract++)
+	{
+	}
+
+	for (i = (nombreCaract - 1), nombreCaract = 0 ; i >= 0 ; i--, nombreCaract++)
+		temporaire[i] = s[nombreCaract];
+	
+	for (i = 0; i < nombreCaract; i++)
+		s[i] = temporaire[i];
+}

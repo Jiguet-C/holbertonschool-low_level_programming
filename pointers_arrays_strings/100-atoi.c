a68cccdea48e5be1b96a454ebae534a0e88fba44
Jiguet-c <catherine.jiguet@gmail.com>
2024-02-29 16:42:47 +0100

    add 100-

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _atoi - convert a string into an integer
+* @s: string
+* Return: integer of the string
+*/
+
+int _atoi(char *s)
+{
+	int i;
+	int nombreCaract;
+
+	for (nombreCaract = 0 ; src[nombreCaract] != '\0' ; nombreCaract++)
+	{
+	}
+
+	for (i = 0; i < nombreCaract; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}

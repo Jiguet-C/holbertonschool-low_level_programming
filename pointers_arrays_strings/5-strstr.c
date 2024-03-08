980a89f1b0e40a3ad673f2c39ebee94d0c03fe77
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-08 14:22:50 +0100

    add 5- empty

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the beginning of the located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	_putchar('a');
+}

6e8c18df249a4d488b0df7b2649465892169f495
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-08 14:32:43 +0100

    add 8- empty

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of the two diagonals of a square matrix of integers
+ * @a: string
+ * @size: array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	printf("%d %d", size, *a);
+}

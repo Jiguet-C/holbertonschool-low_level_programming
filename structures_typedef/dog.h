525e6416d4f95f3e65e3e2362e427f4ff2b4ad88
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-14 16:16:17 +0100

    fix betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
+#endif

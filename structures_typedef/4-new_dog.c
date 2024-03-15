1739fe148de4217e3680dc12cc73b8f998dceb40
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-15 20:52:54 +0100

    fix strdup

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,52 @@
 #include <string.h>
 
 /**
+* _strlen - length of a string
+* @s: string
+* Return: number of char
+*/
+
+int _strlen(char *s)
+{
+	int nombreCaract = 0;
+
+	while (*s != '\0')
+	{
+		nombreCaract++;
+		s++;
+	}
+
+	return (nombreCaract);
+}
+
+/**
+ * _strdup - returns a pointer to a newly memory - copy of the string
+ * @str: string
+ * Return: NULL if size 0 or it fails
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int size;
+	char *buffer;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = _strlen(str) + 1;
+	buffer = malloc(sizeof(unsigned char) * size);
+
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		buffer[i] = str[i];
+
+	return (buffer);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -19,8 +65,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
+	new_dog->name = _strdup(name);
+	new_dog->owner = _strdup(owner);
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
 		free(new_dog->name);

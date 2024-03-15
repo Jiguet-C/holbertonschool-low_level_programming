0f17626a50633a06bedd63de84f8a5ac94fb7a28
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-15 20:25:09 +0100

    add 5-

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to structur
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	free(d);
+}

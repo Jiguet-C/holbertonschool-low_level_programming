3b23c840beda1f06e3df7c1d32c0795b88888f05
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-21 21:18:03 +0100

    fix bug

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,17 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 	va_list numbers;
 
-	if (n != 0)
-	{
-		va_start(numbers, n);
+	va_start(numbers, n);
 
-		for (index = 0; index < n; index++)
-		{
-			printf("%d", va_arg(numbers, int));
-			if (separator != NULL && index != n - 1)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(numbers);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(numbers, int));
+		if (separator != NULL && index != n - 1)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(numbers);
 }

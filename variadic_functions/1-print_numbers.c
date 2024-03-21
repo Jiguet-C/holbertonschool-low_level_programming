0b2b282a9417b376262d840b8e9ce94a33ed5493
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-21 20:49:33 +0100

    simplification

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,20 +10,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int num;
 	unsigned int index;
 	va_list numbers;
 
 	if (n != 0)
 	{
-		num = 0;
 		va_start(numbers, n);
 
 		for (index = 0; index < n; index++)
 		{
-			num = va_arg(numbers, int);
-			printf("%d", num);
-			if (separator != NULL || index < n - 1)
+			printf("%d", va_arg(numbers, int));
+			if (separator != NULL && index < n - 1)
 				printf("%s", separator);
 		}
 		printf("\n");

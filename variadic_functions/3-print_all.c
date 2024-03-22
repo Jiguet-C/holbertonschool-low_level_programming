faff581a475997b0f7932c81a9bae04e3200b74c
Jiguet-c <catherine.jiguet@gmail.com>
2024-03-22 13:54:19 +0100

    add 3-

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything, followed by a new line
+ * @format: list of types of arguments
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int index;
+	char caracterFormat;
+	char *ptr_string;
+	va_list anything;
+
+	va_start(anything, format);
+
+	index = 0;
+
+	while (format != NULL && format[index] != '\0')
+	{
+		caracterFormat = format[index];
+		switch (caracterFormat)
+		{
+			case 'c':
+				printf("%c", va_arg(anything, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(anything, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(anything, double));
+				break;
+			case 's':
+				ptr_string = va_arg(anything, char *);
+				if (ptr_string == NULL)
+					printf("(nil)");
+				else
+					printf("%s", ptr_string);
+				break;
+		}
+		if (format[index + 1] != '\0')
+			printf(", ");
+		index++;
+	}
+	printf("\n");
+	va_end(anything);
+}

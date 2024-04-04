2b4cca6242bec6c299994cc7baa2b49a7197356e
Jiguet-c <catherine.jiguet@gmail.com>
2024-04-04 14:31:31 +0200

    Add 1-

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - length of linked list
+ * @h: list_t list
+ *
+ * Return: number of elements in list_t list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	/* Initialize the counter for nodes */
+	size_t nodes = 0;
+
+	/* Traverse the list until we reach the end */
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	/* Return the total number of nodes in the list */
+	return (nodes);
+}
